b91925a92e5be47f1a3ba08a768c4f099c6bda2e
taylor <taylor@gmail.com>
2025-09-16 16:55:07 +0200

    %date% - %time:~0,5%

---
+++ 1.2. Primeros Pasos JS/Ejercicio 1/script.js
@@ -0,0 +1,15 @@
+var nombreVar='variable1';
+let nombreLet='variable2';
+const nombreConst='variable3';
+
+console.log(nombreVar, ' ', nombreLet, ' ', nombreConst);
+
+nombreVar='cambio1';
+nombreLet='cambio2';
+// nombreConst=cambio3;
+
+console.log(nombreVar, ' ', nombreLet, ' ', nombreConst);
+
+/**
+ * Al intentar cambiar una constante peta
+*/
---
+++ 1.2. Primeros Pasos JS/Ejercicio 5/script.js
@@ -0,0 +1,7 @@
+var resultado=0;
+for (let i = 0; i < 50; i++) {
+    if (i%4==0) {
+        resultado=i
+    }
+}
+console.log(resultado);
