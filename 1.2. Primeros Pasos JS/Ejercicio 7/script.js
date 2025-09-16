dc27b7dd2f907ff7c10026da658a183e4978c5e3
taylor <taylor@gmail.com>
2025-09-16 18:01:44 +0200

    " nuevo commit"

---
+++ 1.2. Primeros Pasos JS/Ejercicio 7/script.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 20; i++) {
+
+    for (let j = 0; j < 20; j++) {
+        if (i != j) {
+            process.stdout.write(' ');
+
+        } else {
+            process.stdout.write('*');
+        }
+
+    }
+    console.log();
+}
---
+++ 1.2. Primeros Pasos JS/Ejercicio 8/script.js
@@ -0,0 +1,13 @@
+for (let i = 20; i >= 0; i--) {
+
+    for (let j = 0; j < 20; j++) {
+        if (i != j) {
+            process.stdout.write(' ');
+
+        } else {
+            process.stdout.write('*');
+        }
+
+    }
+    console.log();
+}
