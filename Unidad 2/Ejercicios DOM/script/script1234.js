3f3ac001ae55bbacd574f7278dc04fe090c507ba
Taylor <taylorher0601@gmail.com>
2025-09-18 17:14:29 +0200

    nuevos ejercicios!

---
+++ Unidad 2/Ejercicios DOM/script/script1234.js
@@ -0,0 +1,40 @@
+// Ejercicio 1
+alert("¡Bienvenide!");
+
+// Ejercicio 2
+var name = prompt("¿Cuál es tu nombre?");
+if (name == "null" || name == "") {
+    alert("Accediste de forma anónima.");
+} else {
+    alert("¡Bienvenide " + name + "!");
+}
+
+// Ejercicio 3
+var num1;
+do {
+    num1 = prompt("Indicame un número: ");
+    num1 = parseInt(num1);
+} while (isNaN(num1));
+
+
+var num2;
+do {
+    num2 = prompt("Indicame un número: ");
+    num2 = parseInt(num2);
+} while (isNaN(num2));
+
+alert("La suma de " + num1 + " y " + num2 + " es " + (num1 + num2));
+
+
+// Ejercicio 4
+var age;
+do {
+    age = prompt("Indicame tu edad: ");
+    age = parseInt(age);
+} while (isNaN(age));
+
+if (age >= 18) {
+    alert("Eres mayor de edad");
+} else {
+    alert("Eres menor de edad");
+}
---
+++ Unidad 2/Ejercicios DOM/script/script5.js
@@ -0,0 +1,10 @@
+// Ejercicio 5.1
+var products = document.getElementById("productos");
+var prices = products.getElementsByClassName("precio");
+var num = prices.length;
+
+//alert("Hay "+num+" precios en los productos.");
+
+// Ejercicio 5.2
+var firstProduct = products.getElementById("p1").innerHTML;
+console.log(firstProduct.inn);
