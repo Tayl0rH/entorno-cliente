d88b762f6ee23e0809c6421c455a18d1cdff12dd
Taylor <taylorher0601@gmail.com>
2025-09-22 16:38:19 +0200

    editando codigo

--- Unidad 2/Ejercicios DOM/script/script1234.js
+++ Unidad 2/Ejercicios DOM/script/script1234.js
@@ -12,14 +12,14 @@ if (name == "null" || name == "") {
 // Ejercicio 3
 var num1;
 do {
-    num1 = prompt("Indicame un número: ");
+    num1 = prompt("Indicame un número 1: ");
     num1 = parseInt(num1);
 } while (isNaN(num1));
 
 
 var num2;
 do {
-    num2 = prompt("Indicame un número: ");
+    num2 = prompt("Indicame un número 2: ");
     num2 = parseInt(num2);
 } while (isNaN(num2));
 
@@ -33,8 +33,10 @@ do {
     age = parseInt(age);
 } while (isNaN(age));
 
-if (age >= 18) {
-    alert("Eres mayor de edad");
-} else {
-    alert("Eres menor de edad");
-}
+//if (age >= 18) {
+//    alert("Eres mayor de edad");
+//} else {
+//    alert("Eres menor de edad");
+//}
+
+alert( age >= 18 ? "Eres mayor de edad" : "Eres menor de edad"); 
