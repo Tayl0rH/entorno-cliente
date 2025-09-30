c1acf6fc637acbf0c23696da45a33f73911bb7b9
Taylor <taylorher0601@gmail.com>
2025-09-30 18:02:25 +0200

    fin ejercicio de estructura básica

--- Unidad 3/3.2 Arrays_ métodos/script.js
+++ Unidad 3/3.2 Arrays_ métodos/script.js
@@ -46,4 +46,55 @@ numbers = new Array(4, 21, 33, 12, 9, 54);
 justPairs = numbers.filter(function(pairs) {
     return pairs%2 == 0
 })
-console.log(justPairs);
+// console.log(justPairs);
+
+// Ejercicio 7
+newLetters = new Array  ('a', 'b', 'c', 'd');
+// console.log(newLetters.includes('c') ? 'Si contiene "C"' : 'No contiene "C"');
+// console.log(newLetters.includes('z') ? 'Si contiene "Z"' : 'No contiene "Z"');
+
+// Ejercicio 8
+let data = [
+{name: "Nacho", telephone: "966112233", age: 40},
+{name: "Ana", telephone: "911223344", age: 35},
+{name: "Mario", phone: "611998877", age: 15},
+{name: "Laura", telephone: "633663366", age: 17}
+];
+
+    // A
+data.push({name: "Pedro", telephone: "611944444", age: 25},);
+data.push({name: "Julia", phone: "633232323", age: 37});
+
+    // B
+// console.log(data);
+
+    // C
+//data.sort(function (a, b){
+//    return a.age - b.age;
+//});
+// console.log(data);
+
+    // D
+data.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+});
+// console.log(data);
+
+    // E
+newData = data.filter(function (person){
+    return person.age >= 30 ;
+});
+// console.log(newData);
+
+// Ejercicio 9
+let products = [
+{ nombre: "Laptop", categoria: "Electrónica", precio: 1500 },
+{ nombre: "Silla", categoria: "Muebles", precio: 100 },
+{ nombre: "Cargador", categoria: "Electrónica", precio: 50 }
+];
+
+chair = products.filter (function (product){
+    return product.nombre =='Silla'
+})
+
+console.log(chair);
