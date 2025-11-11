8a895e4b691b7116c3e47e240d25d48f428b6ce3
Taylor <taylorher0601@gmail.com>
2025-11-11 21:07:05 +0100

    continuamos y arreglamos el botton enabled

--- ProyectoVainilla/scripts/script3.js
+++ ProyectoVainilla/scripts/script3.js
@@ -1,30 +1,39 @@
-document.getElementById('back').addEventListener('click', (e)=>{
+document.getElementById('back').addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = '/ProyectoVainilla/pag2.html'
 })
 
-let question = document.getElementById('question');
-let questionValue;
-question.addEventListener('blur', ()=>{
-    questionValue=question.value;
-    console.log(questionValue);
-});
+let butonOn = false;
 
-let mark = document.getElementById('mark');
-let markValue;
-mark.addEventListener('blur', ()=>{
-    // digito entre 0 y 9
-    markValue=mark.value;
-    console.log(markValue);
-});
+function validQuestionary() {
 
-let trueorfalse = document.getElementById('trueorfalse');
-let trueorfalseValue;
-trueorfalse.addEventListener('blur', ()=>{
-    trueorfalseValue=trueorfalse.value;
-    console.log(trueorfalseValue);
-    if(questionValue && markValue && trueorfalseValue){
-        // enable button
+    let question = document.getElementById('question');
+    let questionValue = question.value;
+
+    let mark = document.getElementById('mark');
+    let markValue = mark.value;
+
+    let trueorfalse = document.getElementById('trueorfalse');
+    let trueorfalseValue = trueorfalse.value;
+    return new Promise((resolved, rejected) => {
+        if (questionValue && markValue && trueorfalseValue) {
+            butonOn = true;
+            resolved();
+        }
+        else rejected(Error('Faltan campos a rellenar'));
+    })
+}
+
+document.addEventListener('keypress', (e) => {
+
+    if (e.key === 'Enter') {
+        validQuestionary()
+            .then((response) => {
+                document.getElementById('save').disabled = false;
+
+            })
+            .catch((error) => {
+                document.getElementById('box').textContent += error.message;
+            })
     }
-});
-
+})
