2c4845c24acca201172bcd2310b956c7ff16dc62
taylor <taylorher0601@gmail.com>
2025-11-14 12:44:13 +0100

    por fin vemor luz con el local storage

--- ProyectoVainilla/scripts/script2.js
+++ ProyectoVainilla/scripts/script2.js
@@ -1,13 +1,18 @@
-// conseguir los datos de usuarios anteriores en caso de ser necesario
+// Así conseguimos el usuario actual
+const currentUser = localStorage.getItem('currentUser');
 
-const savedUserData =  JSON.parse(localStorage.getItem('userData'));
+// y así conseguimos todos los usuarios con sus datos
+// para poder conseguir los del usuario actual
+const savedUserData =  JSON.parse(localStorage.getItem('users'));
 
 
+// Mostramos los datos del usuario en el div correspondiente.
 document.getElementById('welcomeDiv')
-    .innerHTML = 'Hola '+ savedUserData.username +'<br>'+
-    'La última vez que entraste<br>fue el '+ savedUserData.currentDate+
-    '<br>a las '+savedUserData.currentHour;
+    .innerHTML = 'Hola '+ savedUserData[currentUser].username +'<br>'+
+    'La última vez que entraste<br>fue el '+ savedUserData[currentUser].lastDay+
+    '<br>a las '+savedUserData[currentUser].lastHour;
 
+    
 document.getElementById('questions').addEventListener('click', ()=>{
     window.location.href='/ProyectoVainilla/questions.html';
 })
